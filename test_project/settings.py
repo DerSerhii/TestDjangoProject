0c0e9eb7df969ca39dee89003acbbd7b78279fde
serhii <proektssd@gmail.com>
2023-07-10 11:49:45 +0300

    add poetry settings

--- test_project/settings.py
+++ test_project/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
+        'NAME': 'testproject',
         'USER': 'myuser',
         'PASSWORD': 'mypass',
         'HOST': 'localhost',
